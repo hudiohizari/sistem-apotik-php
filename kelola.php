1b0cae675337b0602a02fa34a6b4abe07cd6640e
Hudio Hizari <hhizari@gmail.com>
2018-10-06 15:01:00 +0700

    Menyelesaikan kelola pelanggan

--- kelola.php
+++ kelola.php
@@ -19,6 +19,7 @@ error_reporting(0);
 				updateManusia('dokter', $_POST['primary'], $_POST['nama'], $_POST['no_telp'], $_POST['jenis_kelamin'], $_POST['alamat']);			
 				header("Location: kelola.php?kelola=dokter");
 			}
+
 			if($_POST['submit'] == "Tambah Karyawan"){
 				insertManusia('karyawan', $_POST['nama'], $_POST['no_telp'], $_POST['jenis_kelamin'], $_POST['alamat']);
 				header("Location: kelola.php?kelola=karyawan");
@@ -26,6 +27,15 @@ error_reporting(0);
 			else if($_POST['submit'] == "Edit Karyawan"){
 				updateManusia('karyawan', $_POST['primary'], $_POST['nama'], $_POST['no_telp'], $_POST['jenis_kelamin'], $_POST['alamat']);			
 				header("Location: kelola.php?kelola=karyawan");
+			}
+
+			if($_POST['submit'] == "Tambah Pelanggan"){
+				insertManusia('pelanggan', $_POST['nama'], $_POST['no_telp'], $_POST['jenis_kelamin'], $_POST['alamat']);
+				header("Location: kelola.php?kelola=pelanggan");
+			}
+			else if($_POST['submit'] == "Edit Pelanggan"){
+				updateManusia('pelanggan', $_POST['primary'], $_POST['nama'], $_POST['no_telp'], $_POST['jenis_kelamin'], $_POST['alamat']);			
+				header("Location: kelola.php?kelola=pelanggan");
 			}
 		}
 		else if(isset($_GET['hapus'])) {
@@ -213,8 +223,95 @@ error_reporting(0);
 				<a href="index.php">kembali ke Index</a>
 				<?php
 			}
-			else if($_GET['kelola'] == "pelanggan"){
-	
+			else if($_GET['kelola'] == "pelanggan" || $_GET['kelola'] == "edit_pelanggan"){
+				if ($_GET['kelola'] == "edit_pelanggan"){
+					$res = getSingleManusia('pelanggan', $_GET['primary']);
+					$manusia = mysqli_fetch_array($res);
+				}
+				?>
+				<form action="kelola.php" method="post">
+					<?php
+					if($_GET['kelola'] == "pelanggan"){
+						?>
+						<h3 style="margin-bottom:10px;display:inline">Tambah Pelanggan</h3>
+						<?php
+					}
+					else{
+						?>
+						<h3 style="margin-bottom:10px;display:inline">Edit Pelanggan</h3>
+						<input type="hidden" name="primary" size="25" value="<?php echo $manusia['no_primary']; ?>">
+						<?php
+					}
+					?>
+					<table border="0">
+						<tr>
+							<td>Nama</td>
+							<td><input type="text" name="nama" size="25" value="<?php echo $manusia['nama']; ?>"></td>
+						</tr>
+						<tr>
+							<td>No. Telpon</td>
+							<td><input type="text" name="no_telp" size="25" value="<?php echo $manusia['no_telp']; ?>"></td>
+						</tr>
+						<tr>
+							<td>Jenis Kelamin</td>
+							<td>
+								<select name="jenis_kelamin">
+									<option value="laki - laki">laki - laki</option>
+									<option value="perempuan">perempuan</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>Alamat</td>
+							<td><textarea name="alamat"><?php echo $manusia['alamat']; ?></textarea></td>
+						</tr>
+						<tr>
+							<?php
+							if($_GET['kelola'] == "pelanggan"){
+								?>
+								<td><input type="submit" value="Tambah Pelanggan" name="submit"></td>
+								<?php
+							}
+							else{
+								?>
+								<td><input type="submit" value="Edit Pelanggan" name="submit"></td>
+								<?php
+							}
+							?>
+						</tr>
+					</table>
+				</form>
+				<h3 style="margin-bottom:0;margin-top:20px;">Daftar Pelanggan</h3>
+				<table border="1">
+					<tr>
+						<td>Nomor Pelanggan</td>
+						<td>Nama Pelanggan</td>
+						<td>No. Telpon</td>
+						<td>Jenis Kelamin</td>
+						<td>Alamat</td>
+						<td>Aksi</td>
+					</tr>
+					<?php
+						$result = getAllManusia("pelanggan");
+						while($row = mysqli_fetch_array($result)) {
+							echo '
+							<tr>
+								<td>'.$row["no_primary"].'</td>
+								<td>'.$row["nama"].'</td>
+								<td>'.$row["no_telp"].'</td>
+								<td>'.$row["jenis_kelamin"].'</td>
+								<td>'.$row["alamat"].'</td>
+								<td>
+									<a href="kelola.php?kelola=edit_pelanggan&primary='.$row["no_primary"].'">edit</a> |
+									<a href="kelola.php?hapus=pelanggan&primary='.$row["no_primary"].'">hapus</a>
+								</td>
+							</tr>
+							';
+						}
+					?>
+				</table>
+				<a href="index.php">kembali ke Index</a>
+				<?php
 			}
 			else if($_GET['kelola'] == "obat"){
 	
